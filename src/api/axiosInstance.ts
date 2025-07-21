a699333b98af80b933cf89d71149bcef94a9cc7e
Bunty0409 <122511969+Bunty0409@users.noreply.github.com>
2025-07-21 11:12:22 +0530

    env

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { BASE_URL } from "./apiEndpoints";
 
-const token = import.meta.env.VITE_GITHUB_TOKEN;
+// const token = import.meta.env.VITE_GITHUB_TOKEN;
+const token = "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";
 
 
 const axiosInstance = axios.create({
