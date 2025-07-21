4e5281df93ebf46118497c3f8b9371cf7013c672
Bunty0409 <122511969+Bunty0409@users.noreply.github.com>
2025-07-21 12:25:44 +0530

    update req

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,14 +1,9 @@
 import axios from "axios";
 import { BASE_URL } from "./apiEndpoints";
 
-// const token = import.meta.env.VITE_GITHUB_TOKEN;
-const token = "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";
-
-
 const axiosInstance = axios.create({
     baseURL: `${BASE_URL}`,
     headers: {
-        Authorization: `token ${token}`,
         Accept: "application/vnd.github.v3+json",
     },
 });
