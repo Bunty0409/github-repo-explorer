6a4043520a16ee73023c0d45b3194a1e6419bdae
Bunty0409 <122511969+Bunty0409@users.noreply.github.com>
2025-07-21 11:31:18 +0530

    update token

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./apiEndpoints";
 
 // const token = import.meta.env.VITE_GITHUB_TOKEN;
-const token = "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";
+const token = "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";
 
 
 const axiosInstance = axios.create({
