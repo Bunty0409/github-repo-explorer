16b1880bbce98e62eeec9f771acbc9630635946b
Bunty javia <122511969+Bunty0409@users.noreply.github.com>
2025-07-21 05:37:06 +0000

    add token

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 import { BASE_URL } from "./apiEndpoints";
 
-const token = import.meta.env.VITE_GITHUB_TOKEN;
+const token = "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";
 
 const axiosInstance = axios.create({
-    baseURL: `${BASE_URL}`,
-    headers: {
-        Authorization: `token ${token}`,
-        Accept: "application/vnd.github.v3+json",
-    },
+  baseURL: `${BASE_URL}`,
+  headers: {
+    Authorization: `token ${token}`,
+    Accept: "application/vnd.github.v3+json",
+  },
 });
 
 export default axiosInstance;
